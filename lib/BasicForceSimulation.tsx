f1fab0dc807c9eae68badda754135d54ea3f01ad
Sagar Mohite <sagar.mohite@live.com>
2019-01-22 16:28:50 -0500

    simplify interfaces even further, introduce BasicForceSimulation

--- lib/BasicForceSimulation.tsx
+++ lib/BasicForceSimulation.tsx
@@ -1,71 +1,91 @@
 import * as d3 from 'd3'
-import {VisualGraphData, VisualGraphLink, VisualGraphNode} from './GraphVisualization'
-import {SimulationInterface} from './SimulationInterface'
 
-type D3Simulation = d3.Simulation<VisualGraphNode, VisualGraphLink>
+interface ForceSimulationNode extends d3.SimulationNodeDatum {
+  id: string
 
-const flattenLinks = (links: VisualGraphLink[]) =>
-  links.map(l => ({
-    ...l,
-    source: l.source.id,
-    target: l.target.id,
-  }))
+  /**
+   * d3 forceX seed value
+   * @see https://github.com/d3/d3-force#forceX
+   */
+  forceX?: number
 
-export class BasicForceSimulation implements SimulationInterface {
-  // constructor(data: )
+  /**
+   * d3 forceY seed value
+   * @see https://github.com/d3/d3-force#forceY
+   */
+  forceY?: number
+
+}
+
+interface ForceSimulationLink {
+  source: string
+  target: string
+}
+
+interface ForceSimulationData {
+  nodes: ForceSimulationNode[]
+  links: ForceSimulationLink[]
+}
+
+type D3Simulation = d3.Simulation<d3.SimulationNodeDatum, ForceSimulationLink>
+
+function getForceLinkDistance(links: {source: string, target: string}[]) {
+  // NOTE: For now this is set heuristically on visual appearance/performance of large graphs.
+  // We should ideally measure performance & appearance and tweak this accordingly.
+  const dropoff = 30 - (links.length / 50)
+  return Math.max(dropoff, 0.3)
+}
+
+interface NodePosition {
+  id: string
+  x: number
+  y: number
+  z?: number
+}
+
+export class BasicForceSimulation {
   private simulation: D3Simulation
-  private tickEventHandler(graphData: VisualGraphData) {
+
+  private tickEventHandler(nodePositions: NodePosition[]) {
     // Default no op implementation. Will be overwritten by the callback provided to onSimulationTick
     return
   }
 
-  private getForceLinkDistance(
-    links: {source: string, target: string}[],
-  ) {
-    // NOTE: For now this is set heuristically on visual appearance/performance of large graphs.
-    // We should ideally measure performance & appearance and tweak this accordingly.
-    const dropoff = 30 - (links.length / 50)
-    return Math.max(dropoff, 0.3)
-  }
-
-  public getVisualGraph(): VisualGraphData {
-    const linkForces = this.simulation.force('links') as d3.ForceLink<VisualGraphNode, VisualGraphLink>
-
-    return {
-      nodes: this.simulation.nodes() as VisualGraphNode[],
-      links: linkForces.links() as VisualGraphLink[],
-    }
-  }
-
-  public initialize(graph: VisualGraphData) {
-    const linksWithIds = flattenLinks(graph.links)
-
-    const linkForceDistance = this.getForceLinkDistance(linksWithIds)
+  constructor(graph: ForceSimulationData) {
+    const linkForceDistance = getForceLinkDistance(graph.links)
 
     this.simulation = d3.forceSimulation(graph.nodes)
     // TODO figure out how to conditionally apply force in x direction based on node value
     // (because apply force in direction of 0 is not what we want!)
-      .force('x', d3.forceX().x((node: VisualGraphNode) => node.forceX || 0))
-      .force('y', d3.forceY().y((node: VisualGraphNode) => node.forceY  || 0))
-      .force('links', d3.forceLink(linksWithIds).id((n: VisualGraphNode) => n.id).distance(linkForceDistance))
+      .force('x', d3.forceX().x((node: ForceSimulationNode) => node.forceX || 0))
+      .force('y', d3.forceY().y((node: ForceSimulationNode) => node.forceY || 0))
+      .force('links', d3.forceLink(graph.links).id((n: ForceSimulationNode) => n.id).distance(linkForceDistance))
       .force('charge', d3.forceManyBody().strength(-200))
       .velocityDecay(0.7)
       .on('tick', () => {
-        const visualGraph = this.getVisualGraph()
-        this.tickEventHandler(visualGraph)
+        this.tickEventHandler(this.getPositions())
       })
   }
 
-  public onSimulationTick(callback: (graphData: VisualGraphData) => void) {
+  public getPositions(): NodePosition[] {
+    const nodes = this.simulation.nodes() as ForceSimulationNode[]
+
+    return nodes.map(node => ({
+      id: node.id,
+      x: node.x || 0,
+      y: node.y || 0,
+    }))
+  }
+
+  public onSimulationTick(callback: (nodePositions: NodePosition[]) => void) {
     this.tickEventHandler = callback
   }
 
-  public update(graph: VisualGraphData) {
-    const linksWithIds = flattenLinks(graph.links)
-    const linkForceDistance = this.getForceLinkDistance(linksWithIds)
+  public update(graph: ForceSimulationData) {
+    const linkForceDistance = getForceLinkDistance(graph.links)
     this.simulation
       .nodes(graph.nodes)
-      .force('links', d3.forceLink(linksWithIds).id((n: VisualGraphNode) => n.id).distance(linkForceDistance))
+      .force('links', d3.forceLink(graph.links).id((n: ForceSimulationNode) => n.id).distance(linkForceDistance))
   }
 
   public restart() {
