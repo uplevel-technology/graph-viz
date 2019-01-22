7d2a0798fa8b96c76bb2db3dfdb658202e69d58f
Sagar Mohite <sagar.mohite@live.com>
2019-01-22 13:34:27 -0500

    flatten unnecessary namespaces, make interface names specific instead

--- lib/SimulationInterface.ts
+++ lib/SimulationInterface.ts
@@ -47,107 +47,107 @@ export interface SimulationLink {
   target: string
 }
 
+export interface SimulationInputNode {
+  /**
+   * unique node id
+   */
+  id: string
 
-// Alternatively
-namespace Simulation {
-  export namespace Input {
-    export interface Node {
-      /**
-       * unique node id
-       */
-      id: string
+  /**
+   * initial seed x position. Should w name this as
+   */
+  x?: number
 
-      /**
-       * initial seed x position. Should w name this as
-       */
-      x?: number
-
-      /**
-       * initial seed y position
-       */
-      y?: number
-      fixed?: boolean
-    }
-    export interface Link {
-      source?: string
-      target?: string
-    }
-    export interface Graph {
-      nodes: Node[],
-      links: Link[]
-    }
-  }
-
-  export namespace Output {
-    export interface Node {
-      id: string
-      x: number
-      y: number
-      fixed?: boolean
-    }
-    export interface Link {
-      source: string
-      target: string
-    }
-    export interface Graph {
-      nodes: Node[],
-      links: Link[]
-    }
-  }
+  /**
+   * initial seed y position
+   */
+  y?: number
+  fixed?: boolean
 }
 
-export type SimulateFn = (inputGraph: Simulation.Input.Graph) => Simulation.Output.Graph
-
-namespace Viz {
-  export interface Node {
-    /**
-     * Unique node id
-     */
-    id: string
-
-    /**
-     * inactive is a boolean that makes a node grey when set.
-     * TODO: this has to be deprecated soon. see TODO above.
-     */
-    inactive?: boolean
-
-    /**
-     * node fill color hex string or hex number
-     */
-    fill?: number | string
-
-    /**
-     * The node container's absolute size in pixels at the default zoom level
-     * TODO: This is a bad name. We need to make more sense of this by ensuring all visual attributes
-     * can be translated represented by a property name in VisualGraphNode. e.g. scale.
-     */
-    size?: number
-
-    /**
-     * node strike color hex string or hex number
-     */
-    stroke?: number | string
-
-    /**
-     * relative node stroke opacity (must be between 0.0 to 1.0)
-     */
-    strokeOpacity?: number
-
-    /**
-     * relative node stroke width (must be between 0.0 to 1.0)
-     */
-    strokeWidth?: number
-  }
-
-  export interface Link {
-    source: string
-    target: string
-
-    /**
-     * determines whether an arrow is drawn on the link
-     */
-    directed?: string
-  }
+export interface SimulationOutputNode {
+  id: string
+  x: number
+  y: number
+  fixed?: boolean
 }
 
-type Node = Viz.Node & Simulation.Input.Node
+export interface SimulationLink {
+  source: string
+  target: string
+}
+
+export interface SimulationInput {
+  nodes: SimulationInputNode[],
+  links: SimulationLink[],
+}
+
+export interface SimulationOutput {
+  nodes: SimulationOutputNode[],
+  links: SimulationLink[],
+}
+
+export type SimulateFn = (inputGraph: SimulationInput) => SimulationOutput
+
+export interface VisualNode {
+  /**
+   * Unique node id
+   */
+  id: string
+
+  /**
+   * inactive is a boolean that makes a node grey when set.
+   * TODO: this has to be deprecated soon. see TODO above.
+   */
+  inactive?: boolean
+
+  /**
+   * node fill color hex string or hex number
+   */
+  fill?: number | string
+
+  /**
+   * The node container's absolute size in pixels at the default zoom level
+   * TODO: This is a bad name. We need to make more sense of this by ensuring all visual attributes
+   * can be translated represented by a property name in VisualGraphNode. e.g. scale.
+   */
+  size?: number
+
+  /**
+   * node strike color hex string or hex number
+   */
+  stroke?: number | string
+
+  /**
+   * relative node stroke opacity (must be between 0.0 to 1.0)
+   */
+  strokeOpacity?: number
+
+  /**
+   * relative node stroke width (must be between 0.0 to 1.0)
+   */
+  strokeWidth?: number
+}
+
+export interface VisualLink {
+  source: string
+  target: string
+
+  /**
+   * determines whether an arrow is drawn on the link
+   */
+  directed?: string
+
+  /**
+   * hex color string or hex number
+   */
+  color: string | number
+}
+
+type GraphNode = VisualNode & SimulationOutputNode
+type GraphLink = VisualLink & SimulationLink
+
+interface GraphData {
+  nodes: GraphNode[]
+  links: GraphLink[]
+}
