954dd10d338e247caa0c5c1b7ae7e2a18e62c0c0
Sagar Mohite <sagar.mohite@live.com>
2019-04-03 12:07:53 -0400

    define GraphVizCluster interface

--- lib/Clusters.ts
+++ lib/Clusters.ts
@@ -4,6 +4,26 @@ import * as THREE from 'three'
 import {MeshBasicMaterial} from 'three'
 import {get2DConvexHull, getNiceOffsetPolygon} from './convexHull'
 
+interface GraphVizCluster {
+  id: string | number
+  /**
+   * node fill color hex string or hex number
+   * (default is 0x333333)
+   */
+  fill?: number | string
+
+  /**
+   * relative node fill opacity
+   * (must be between 0.0 - 1.0)
+   */
+  fillOpacity?: number
+
+  /**
+   * inner padding from the boundary nodes
+   */
+  padding?: number
+}
+
 export class Clusters {
   public object = new THREE.Group()
   private meshes: {[clusterId: string]: THREE.Mesh | THREE.Points} = {}
