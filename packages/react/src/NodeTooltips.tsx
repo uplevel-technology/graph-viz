31b0a90f0db57465ad5745e1403c0e7620b9e665
Sagar Mohite <sagar.mohite@live.com>
2019-08-21 23:41:33 -0400

    minify tooltips

--- packages/react/src/NodeTooltips.tsx
+++ packages/react/src/NodeTooltips.tsx
@@ -4,10 +4,6 @@ import {CSSProperties} from 'react'
 export interface TooltipFields {
   id: string
   displayName: string
-  displayType?: string
-  formattedTime?: string
-  clusterId?: number
-  pattern?: string
 }
 
 export interface TooltipNode extends TooltipFields {
@@ -40,60 +36,17 @@ const styles = {
   },
 }
 
-interface Props {
+interface NodeTooltipsProps {
   node: TooltipNode | null
-  renderTooltip?: Element | (props) = n
 }
 
-export class NodeTooltips extends React.Component<Props> {
-  render() {
-    if (!this.props.node) {
-      return null
-    }
-    return (
-      <div style={{...styles.root, ...getTooltipStyle(this.props.node)}}
-      >
-        <Grid container direction={'row'} justify={'space-between'}>
-          <Grid item>
-            {this.props.node.displayType && (
-              <Typography style={{fontSize: 10, fontWeight: 'bold'}}>
-                {this.props.node.displayType}
-              </Typography>
-            )}
-          </Grid>
-          <Grid item>
-            {this.props.node.clusterId !== undefined && (
-              <Typography
-                style={{
-                  textTransform: 'none',
-                  fontSize: 10,
-                  color: '#555555',
-                }}
-              >
-                {`Cluster ${this.props.node.clusterId!}`}
-              </Typography>
-            )}
-          </Grid>
-        </Grid>
-        <Typography
-          variant={'body2'}
-          style={{...styles.breakLongWord, ...styles.origCase}}
-        >
-          {this.props.node.displayName}
-        </Typography>
-        {this.props.node.formattedTime !== undefined && (
-          <Typography style={{fontSize: 10, color: '#555555'}}>
-            {this.props.node.formattedTime}
-          </Typography>
-        )}
-        {this.props.node.pattern !== undefined && (
-          <Typography
-            style={{fontSize: 10, color: '#555555', textTransform: 'none'}}
-          >
-            {`Matches '${this.props.node.pattern}'`}
-          </Typography>
-        )}
-      </Grid>
-    )
+export function NodeTooltips(props: NodeTooltipsProps) {
+  if (!props.node) {
+    return null
   }
+  return (
+    <div style={{...styles.root, ...getTooltipStyle(this.props.node)}}>
+      {props.node.displayName}
+    </div>
+  )
 }
