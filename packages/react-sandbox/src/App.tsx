00bd2c6d27986a4c5a5abd18b36bea917c65a6b8
sagar-sm <sagar.mohite@live.com>
2019-09-03 13:32:25 -0400

    add react sandbox private package

---
+++ packages/react-sandbox/src/App.tsx
@@ -0,0 +1,50 @@
+import React from 'react'
+import logo from './logo.svg'
+import './App.css'
+import {GraphVizComponent} from '@graph-viz/react'
+import {DisplayGroups} from '@graph-viz/core'
+
+const DATA = {
+  nodes: [
+    {id: '1', fill: 'coral'},
+    {id: '2', fill: 'orange'},
+    {id: '3', displayGroupIds: ['c2'], fill: 'blue'},
+    {id: '4', displayGroupIds: ['c2'], fill: 'lightblue'},
+    {id: '5', displayGroupIds: ['c2'], fill: 'orange'},
+    {id: '6', displayGroupIds: ['c2'], fill: 'green'},
+  ],
+  links: [
+    {source: '1', target: '2'},
+    {source: '3', target: '4'},
+    {source: '3', target: '5'},
+    {source: '3', target: '6'},
+  ],
+  groups: [
+    {
+      id: 'c2',
+      visible: true,
+      shape: 'convexHull' as const,
+    },
+  ],
+}
+
+const App: React.FC = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+      </header>
+      <div className="App-content">
+        <div className="App-graph-viz-container">
+          <GraphVizComponent
+            nodes={DATA.nodes}
+            links={DATA.links}
+            groups={DATA.groups}
+          />
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default App
